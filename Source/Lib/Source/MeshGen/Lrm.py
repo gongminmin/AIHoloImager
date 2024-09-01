[MeshGen] Fix the mesh scaling problem

Source/Lib/Source/MeshGen/Lrm.py
@@ -29,12 +29,12 @@ class Lrm(nn.Module):
                  triplane_dim : int = 80,
                  rendering_samples_per_ray : int = 128,
                  grid_res : int = 128,
-                 grid_scale : float = 2.0,
-                 fovy : float = 50.0):
+                 grid_scale : float = 2.0):
         super(Lrm, self).__init__()
 
         self.device = device
         self.grid_res = grid_res
+        self.grid_scale = grid_scale
 
         this_py_dir = Path(__file__).parent.resolve()
         pretrained_dir = this_py_dir.joinpath("Models/dino-vitb16")
@@ -175,8 +175,7 @@ class Lrm(nn.Module):
         verts, faces = mcubes.marching_cubes(sdf.cpu().numpy(), 0)
 
         verts = torch.tensor(verts, dtype = torch.float32, device = self.device)
-        verts = verts / self.grid_res * 2 - 1
-        verts *= 1.04 # Not sure why we need this scale
+        verts = (verts / self.grid_res - 0.5) * self.grid_scale
 
         faces = torch.tensor(faces.astype(int), dtype = torch.int32, device = self.device)
         # Flip the triangles

Source/Lib/Source/MeshGen/MeshGenerator.py
@@ -10,6 +10,7 @@ from pytorch_lightning import seed_everything
 import torch
 
 from src.utils.camera_util import get_zero123plus_input_cameras
+from Lrm import Lrm
 
 class MeshGenerator:
     def __init__(self):
@@ -21,10 +22,9 @@ class MeshGenerator:
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        from Lrm import Lrm
         self.model = Lrm(self.device, encoder_feat_dim = 768, transformer_dim = 1024, transformer_layers = 16, transformer_heads = 16,
             triplane_low_res = 32, triplane_high_res = 64, triplane_dim = 80, rendering_samples_per_ray = 128, grid_res = 128,
-            grid_scale = 2.1, fovy = 30.0
+            grid_scale = 2.1
         )
 
         model_ckpt_path = this_py_dir.joinpath(f"Models/instant_mesh_large.ckpt")